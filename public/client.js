cfc827aabc1b0d8a5089d2f4d1c6479900d63810
race-briggs <race.briggs@gmail.com>
2018-12-19 17:36:24 -0600

    cleaned up client.js

--- public/client.js
+++ public/client.js
@@ -3,34 +3,34 @@
 let MOCK_API_DATA = {
 	"memeList": [
 		{
-			"name": 'Kappa',
-			"type": 'emoticon',
-			"collection": 'twitch',
-			"origin": 'Twitch'
+			"name": "Kappa",
+			"type": "emoticon",
+			"collection": "twitch",
+			"origin": "Twitch"
 		},
 		{
-			"name": 'Navy Seal',
-			"type": 'copypasta'
-			"collection": 'text'
-			"origin": '4chan'
+			"name": "Navy Seal",
+			"type": "copypasta",
+			"collection": "text",
+			"origin": "4chan"
 		},
 		{
-			"name": 'Moth Memes'
-			"type": 'picture'
-			"collection": 'picture'
-			"origin": 'unknown'
+			"name": "Moth Memes",
+			"type": "picture",
+			"collection": "picture",
+			"origin": "unknown"
 		},
 		{
-			"name": 'Used Car Salesman'
-			"type": 'picture'
-			"collection": 'picture'
-			"origin": 'Reddit'
+			"name": "Used Car Salesman",
+			"type": "picture",
+			"collection": "picture",
+			"origin": "Reddit"
 		},
 		{
-			"name": 'Hell in a Cell'
-			"type": 'text bait'
-			"collection": 'text'
-			"origin": 'Reddit'
+			"name": "Hell in a Cell",
+			"type": "text bait",
+			"collection": "text",
+			"origin": "Reddit"
 		}
 	]	
 };
