28a0edbe1a1eafd2d4f9f9a0f3fc2b96a313d9ef
race-briggs <race.briggs@gmail.com>
2018-12-20 16:53:02 -0600

    added models.js file

---
+++ models.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const memeEntrySchema = mongoose.Schema({
+	name: {type: String, required: true},
+	type: {type: String, required: true},
+	origin: {type: String}
+});
+
+const MemeEntry = {'Meme', memeEntrySchema};
+
+module.exports = {MemeEntry};
