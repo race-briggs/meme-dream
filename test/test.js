308ac782da4ad49ff0b2bc2dad76ffe1a779abb3
race-briggs <race.briggs@gmail.com>
2018-12-23 18:37:30 -0600

    changed test.js file

--- test/test.js
+++ test/test.js
@@ -7,24 +7,57 @@ const mongoose = require("mongoose");
 const expect = chai.expect;
 
 const {app, runServer, closeServer} = require('../server.js');
+const {MemeEntry} = require('../models.js');
 //const {TEST_DATABASE_URL} = require('../config.js');
 
 chai.use(chaiHttp);
 
+function seedMemeData(){
+	console.info('seeding test database');
+	let seedData = [];
+	for(let i = 0; i < 10; i++){
+		seedData.push(generateMemeData());
+	};
+	return MemeEntry.insertMany(seedData);
+};
+
+function generateMemeData(){
+	return {
+		name: faker.name(),
+		type: faker.lorem.word(),
+		origin: faker.lorem.word()
+	};
+};
+
+function tearDown(){
+	console.warn('tearing down database');
+	return mongoose.connection.dropDatabase();
+}
+
 describe("basic test function", function(){
 
-	//before(function(){
-	//	return runServer('http://localhost/meme-dream');
-	//});
+	before(function(){
+		return runServer('http://localhost/meme-dream');
+	});
 
-	//after(function(){
-	//	return closeServer();
-	//});
+	beforeEach(function(){
+		return seedMemeData();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	afterEach(function(){
+		return tearDown();
+	})
 
 	it('should give me a 200 status at the URL', function(){
+		let res;
 		return chai.request(app)
 			.get('/memes')
-			.then(res => {
+			.then(_res => {
+				res = _res;
 				expect(res).to.have.status(200);
 			});
 	});
