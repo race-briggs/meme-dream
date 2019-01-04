85fc4feb2a067c66cce7ab620e10eff76283d607
Race Briggs <race.briggs@gmail.com>
2019-01-04 12:04:28 -0600

    fixing unit testing

--- public/client.js
+++ public/client.js
@@ -1,39 +1,39 @@
 'use strict';
 
-let MOCK_API_DATA = {
-	"memeList": [
-		{
-			"name": "Kappa",
-			"type": "emoticon",
-			"collection": "twitch",
-			"origin": "Twitch"
-		},
-		{
-			"name": "Navy Seal",
-			"type": "copypasta",
-			"collection": "text",
-			"origin": "4chan"
-		},
-		{
-			"name": "Moth Memes",
-			"type": "picture",
-			"collection": "picture",
-			"origin": "unknown"
-		},
-		{
-			"name": "Used Car Salesman",
-			"type": "picture",
-			"collection": "picture",
-			"origin": "Reddit"
-		},
-		{
-			"name": "Hell in a Cell",
-			"type": "text bait",
-			"collection": "text",
-			"origin": "Reddit"
-		}
-	]	
-};
+//let MOCK_API_DATA = {
+//	"memeList": [
+//		{
+//			"name": "Kappa",
+//			"type": "emoticon",
+//			"collection": "twitch",
+//			"origin": "Twitch"
+//		},
+//		{
+//			"name": "Navy Seal",
+//			"type": "copypasta",
+//			"collection": "text",
+//			"origin": "4chan"
+//		},
+//		{
+//			"name": "Moth Memes",
+//			"type": "picture",
+//			"collection": "picture",
+//			"origin": "unknown"
+//		},
+//		{
+//			"name": "Used Car Salesman",
+//			"type": "picture",
+//			"collection": "picture",
+//			"origin": "Reddit"
+//		},
+//		{
+//			"name": "Hell in a Cell",
+//			"type": "text bait",
+//			"collection": "text",
+//			"origin": "Reddit"
+//		}
+//	]	
+//};
 
 function getMemes(callbackFn){
 	setTimeout(function(){callbackFn(MOCK_API_DATA)}, 1);
--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
-
 app.get('/memes', (req, res) => {
 	MemeEntry.find()
 		.then(memes => {
