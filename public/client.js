8f9ada23dce149606c08c6831232421305a40b12
race-briggs <race.briggs@gmail.com>
2019-01-12 17:23:17 -0600

    added styling to index

--- public/client.js
+++ public/client.js
@@ -35,9 +35,36 @@
 //	]	
 //};
 
-function getMemes(callbackFn){
+const searchUrl = "https://sheltered-fortress-34693.herokuapp.com/memes"
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function getMemes(query){
 	setTimeout(function(){callbackFn(MOCK_API_DATA)}, 1);
-};
+
+	const params = {
+
+	};
+
+	url = searchUrl + '?' query;
+
+	fetch(url)
+		.then(response => {
+			if(response.ok) {
+				return response.json();
+			}
+			throw new Error(respnose.statusText);
+		})
+		.then(responseJson => displayMemes(responseJson))
+		.catch(err => {
+			console.error(err);
+		});
+
+}
 
 function displayMemes(data){
 	for(index in data.memeList){
